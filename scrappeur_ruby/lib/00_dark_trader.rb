6bd4ab6007e0a8bdd12e285a63f2109948386bca
Remy-Schreiner <remyschreiner83510@gmail.com>
2025-03-21 01:00:23 +0100

    Update 00_dark_trader.rb

--- scrappeur_ruby/lib/00_dark_trader.rb
+++ scrappeur_ruby/lib/00_dark_trader.rb
@@ -1,39 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
-require 'uri'
 
-base_url = "https://coinmarketcap.com/all/views/all/"
-
-def get_price_crypto(base_url)
-
-    page = Nokogiri::HTML(URI.open(base_url))
-    name_crypto = page.xpath('//a[@class="cmc-table__column-name--name cmc-link"]')
-
-    page = Nokogiri::HTML(URI.open(base_url))
-    price_crypto = page.xpath('//div[contains(@class, "sc-142c02c-0 lmjbLF")]//span')
-
-
-    final_tabl = []
-    tabl_name = []
-    tabl_price = []
-
-
-    price_crypto.each do |i|
-        tabl_price << i.text
-    end
-
-    name_crypto.each do |i|
-        tabl_name << i.text
-    end
-    
-    tabl_name.each_with_index do |name, index|
-        my_hash = Hash.new
-        my_hash[name] = tabl_price [index]
-
-        final_tabl << my_hash
-end
-    return final_tabl
+def get_price_crypto(url)
+  page = Nokogiri::HTML(URI.open(url))
+  
+  names = page.xpath('//a[@class="cmc-table__column-name--name cmc-link"]').map(&:text)
+  prices = page.xpath('//div[contains(@class, "sc-142c02c-0 lmjbLF")]//span').map(&:text)
+  
+  names.zip(prices).map { |name, price| { name => price } }
 end
 
-
-puts get_price_crypto(base_url)
+puts get_price_crypto("https://coinmarketcap.com/all/views/all/")
