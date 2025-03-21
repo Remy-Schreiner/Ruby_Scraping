7fe85a3ef21128fce6c5222048e92712b64cb97f
Remy-Schreiner <remyschreiner83510@gmail.com>
2025-03-21 01:04:12 +0100

    Update 00_dark_trader_spec.rb

--- scrappeur_ruby/spec/00_dark_trader_spec.rb
+++ scrappeur_ruby/spec/00_dark_trader_spec.rb
@@ -1,14 +1,11 @@
-require_relative '../lib/00_dark_trader'
+require_relative '../lib/00_dark_trader' 
+require 'rspec'
 
-RSpec.describe "Scraper des cryptos" do
-  let(:base_url) { "https://coinmarketcap.com/all/views/all/" }
-  let(:crypto_prices) { get_price_crypto(base_url) }
-
-  it "récupère des prix de cryptos" do
-    expect(crypto_prices).to be_an(Array)  # Vérifie que ce n'est pas nil
+RSpec.describe "Crypto Scraper" do
+  it "retrieves cryptocurrency prices" do
+    crypto_prices = get_price_crypto("https://coinmarketcap.com/all/views/all/")
+    expect(crypto_prices).to be_an(Array)
     expect(crypto_prices).not_to be_empty
     expect(crypto_prices.first).to be_a(Hash)
-    expect(crypto_prices.first.keys.first).to be_a(String)
-    expect(crypto_prices.first.values.first).to match(/\d+(\.\d+)?/)
   end
 end
